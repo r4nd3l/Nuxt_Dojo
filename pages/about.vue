d85785ef20aca6b8c0fa27ecea86a09f7ceac565
Mate Molnar <molnar.mate@evista.hu>
2022-11-17 21:57:12 +0100

    Nuxt Dojo basics

---
+++ pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+      perspiciatis enim praesentium vero molestias sequi eius magni, est iure
+      saepe.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+      perspiciatis enim praesentium vero molestias sequi eius magni, est iure
+      saepe.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
